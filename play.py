0c95af3c5fb0512a68e47546b121c1c99a280aae
Bruno Moyle <brunitto@gmail.com>
2017-01-27 18:27:30 -0200

    Development checkpoint

--- play.py
+++ play.py
@@ -5,45 +5,60 @@ import os
 # TODO use lists
 # TODO do not use global variables
 
+session_dungeon = "--"
+session_room = "--"
+session_player_name = "--"
+session_player_hp = "--"
+session_player_ap = "--"
+session_player_dp = "--"
+session_player_gp = "--"
+
 def room_1():
+    global session_dungeon, session_room
+    session_dungeon = "01"
+    session_room = "01"
     clear_screen()
     print_general_screen()
-    print """DM: You wake up in a dark and dry room. You look around and it's
-    looks like a cell. Right in front of you, close to the cell bars, there is a
-    guard, drop dead...
-
-    1 - Scream for help
-    2 - Check the guard body
-    """
+    print_room_description(
+        "You wake up in a dark and dry room. You look around and it's looks\n"
+        "like a cell. Right in front of you, close to the cell bars, there is\n"
+        "a guard, drop dead..."
+    )
+    print_room_options([
+        "Scream for help",
+        "Check the guard's body"
+    ])
 
     option = ""
     while option != "q":
         option = raw_input("Choose an option (q to quit): ")
-        clear_screen()
         if option == "1":
-            print """No one listens to you, you are on your own.
-            """
-            raw_input("Press any key to continue...")
+            print_room_notice("No one listens to you, you are on your own.")
             room_1()
         elif option == "2":
-            print """You find the cell key in his pocket. You leave the cell.
-            """
+            print_room_notice(
+                "You find the cell key in his pocket. You leave the cell"
+            )
             room_2()
         elif option == "q":
             game_over()
         else:
-            print """Choose a valid option!
-            """
+            print "Choose a valid option"
 
 def room_2():
+    global session_dungeon, session_room
+    session_dungeon = "01"
+    session_room = "02"
     clear_screen()
     print_general_screen()
-    print """DM: This look like a corridor full of cells. There is an open door
-    on the right and a long corridor on the left...
-
-    1 - Enter the open door on the right
-    2 - Run through the corridor on the left
-    """
+    print_room_description(
+        "This look like a corridor full of cells. There is an open door on\n"
+        "the right and a long corridor on the left"
+    )
+    print_room_options([
+        "Enter the open door on the right",
+        "Run through the corridor on the left"
+    ])
 
     option = ""
     while option != "q":
@@ -55,93 +70,135 @@ def room_2():
         elif option == "q":
             game_over()
         else:
-            print """Choose a valid option!
-            """
+            print "Choose a valid option"
 
 def room_3():
+    global session_dungeon, session_room
+    session_dungeon = "01"
+    session_room = "03"
     clear_screen()
     print_general_screen()
-    print """DM: You enter a messy and dry room, there are a interesting desk in
-    the corner, covered with papers and a shiny drawer...
-
-    1 - Check the drawer
-    2 - Leave the room
-    """
+    print_room_description(
+        "You enter a messy and dry room, there are a interesting desk in the\n"
+        "corner, covered with papers and a shiny drawer"
+    )
+    print_room_options([
+        "Check the drawer",
+        "Leave the room"
+    ])
 
     option = ""
     while option != "q":
         option = raw_input("Choose an option (q to quit): ")
         if option == "1":
-            print """You found 2GP.
-            """
-            raw_input("Press any key to continue...")
+            global session_player_gp
+            session_player_gp += 2
+            print_room_notice("You found 2GP")
             room_3()
         elif option == "2":
             room_2()
         elif option == "q":
             game_over()
         else:
-            print """Choose a valid option!
-            """
+            print "Choose a valid option"
 
 def room_4():
+    global session_dungeon, session_room
+    session_dungeon = "01"
+    session_room = "04"
     clear_screen()
     print_general_screen()
-    print """DM: You enter the main hall. You fell a huge shade coming from
-    nowhere, weaving a big axe at you, time to battle...
-    """
-
-    print """The battle system is not available!
-    """
-    raw_input("Press any key to continue...")
-
+    print_room_description(
+        "You enter the main hall. You fell a huge shade coming from nowhere\n"
+        "weaving a big axe at you, time to battle"
+    )
+    print_room_notice("The battle system is not available")
     room_5()
 
 def room_5():
+    global session_dungeon, session_room
+    session_dungeon = "01"
+    session_room = "05"
     clear_screen()
     print_general_screen()
-    print """DM: After a heat battle, you still have energy to run. At the end
-    of the hall, there is a dark corridor. When you enter it, you notice that
-    part of the floor looks different...
-
-    1 - Check for traps
-    2 - Run like there is no tomorrow
-    """
+    print_room_description(
+        "After a heat battle, you still have energy to run. At the end of the\n"
+        "hall, there is a dark corridor. When you enter it, you notice that\n"
+        "part of the floor looks different"
+    )
+    print_room_options([
+        "Check for traps",
+        "Run like there is no tomorrow"
+    ])
 
     option = ""
     while option != "q":
         option = raw_input("Choose an option (q to quit): ")
         if option == "1":
-            print """You find a trap, a clumsy deep hole full of pythons. You jump over it.
-            """
+            print_room_notice(
+                "You find a trap, a clumsy deep hole full of pythons. You jump "
+                "over it."
+            )
             game_over()
         elif option == "2":
-            print """You fall in a deep hole and get bitten by the pythons.
-            """
+            print_room_notice(
+                "You fall in a deep hole and get bitten by the pythons."
+            )
             game_over()
         elif option == "q":
             game_over()
         else:
-            print """Choose a valid option!
-            """
+            print "Choose a valid option"
 
 def print_general_screen():
-    print """
-    DUNGEONS AND PYTHONS
-    ----------------------------------------------------------------------------
-    Dungeon: --, Room: --
-    Player: PLAYER NAME, HP: --, AP: --, DP: --, GP: --
-    """
+    print (
+        "DUNGEONS AND PYTHONS\n"
+        "--------------------------------------------------------------------------------\n"
+        "Dungeon: %s, Room: %s\n"
+        "Player: %s, HP: %s, AP: %s, DP: %s, GP: %s\n"
+    ) % (
+        session_dungeon,
+        session_room,
+        session_player_name,
+        session_player_hp,
+        session_player_ap,
+        session_player_dp,
+        session_player_gp
+    )
+
+def print_battle_screen():
+    print ""
 
 def game_over():
-    print """GAME OVER!
-    """
+    print "GAME OVER!\n"
     exit(1)
 
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
+def print_room_description(description):
+    print "DM: %s\n" % description
+
+def print_room_options(options):
+    options_counter = 1
+    for option in options:
+        print "%s - %s" % (options_counter, option)
+        options_counter += 1
+    print ""
+
+def print_room_notice(notice):
+    clear_screen()
+    print "%s\n" % (notice)
+    raw_input("Press any key to continue...")
+
 def start():
+    global session_player_name, session_player_hp, session_player_ap, session_player_dp, session_player_gp
+    print "Welcome to Dungeons and Pythons\n"
+    session_player_name = raw_input("What's your name? ")
+    session_player_hp = 10
+    session_player_ap = 2
+    session_player_dp = 2
+    session_player_gp = 0
     room_1()
 
 start()
